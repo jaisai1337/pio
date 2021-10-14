06309ceee537cae97e34db9a7241b4d2af757727
jaisai1337 <48624947+jaisai1337@users.noreply.github.com>
2021-10-14 11:04:02 +0530

    Update pio_04rpi.sh

--- pio_04rpi.sh
+++ pio_04rpi.sh
@@ -2,7 +2,6 @@ pip3 install platformio
 mkdir -p ~/hi/.pio/build/esp32doit-devkit-v1/
 cd hi
 wget https://raw.githubusercontent.com/gadepall/termux/main/pio/Projects/hi/platformio.ini
-nano platformio.ini
 #comment the following line like this
 #;platform_packages = toolchain-xtensa32@https://github.com/esphome/esphome-docker-base/releases/download/v1.4.0/toolchain-xtensa32.tar.gz
 #ctrl+x y enter
