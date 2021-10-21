d2dc8b06744b62875ade9a1f813bf2096d6730c7
jaisai1337 <48624947+jaisai1337@users.noreply.github.com>
2021-10-21 10:02:55 +0530

    Update pio_02.sh

--- pio_02.sh
+++ pio_02.sh
@@ -1,3 +1,3 @@
 apt update && apt upgrade -y
-apt install apt-utils build-essential
+apt install apt-utils build-essential -y
 apt install git wget subversion imagemagick nano python3-pip python3-numpy python3-scipy python3-matplotlib python3-mpmath python3-sympy python3-cvxopt -y
