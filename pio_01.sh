2c5a9dc182a2037b48310cf9123348d54998463f
jaisai1337 <48624947+jaisai1337@users.noreply.github.com>
2021-10-25 10:44:08 +0530

    Update pio_01.sh

--- pio_01.sh
+++ pio_01.sh
@@ -3,4 +3,4 @@ apt install build-essential -y
 apt install git silversearcher-ag wget openssh subversion imagemagick proot nano python bsdtar mutt nmap proot-distro -y
 apt update
 #Install ubuntu
-#proot-distro install ubuntu 
+proot-distro install ubuntu 
